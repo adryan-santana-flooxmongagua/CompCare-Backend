09e7a211e5d6c8a275dd721ba9b411c124ebe726
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-05-02 08:46:46 -0300

    chore: import and configure base middlewares

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,15 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+// cORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+// Uploads directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// DB connection
+require("./src/config/db.js")
+
 // routes
 const router = require("./src/routes/Router.js");
 app.use(router);
